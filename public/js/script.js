38c9168093a33878a4d7f8fc04dd286ff682d30b
Mathias <mathias.lavergne.graph@gmail.com>
2019-02-19 17:31:46 +0100

    Ajout du fichier script.js

---
+++ public/js/script.js
@@ -0,0 +1,34 @@
+
+
+// document.getElementById("deleteBtn").addEventListener("click", function() {
+//     document.getElementById("overlayDeletePost").style.display = "block";
+// });
+
+// document.getElementsByClassName("listPosts").style.backgroundColor = "blue";
+
+// document.getElementById("overlayDeletePost").style.display = "block";
+
+// function askDeletePost() {
+//     document.getElementById("overlayDeletePost").style.display = "block";
+// }
+
+// var deleteBtn = document.getElementsByClassName("deleteBtn");
+
+// for (var i = 0; i < deleteBtn.length; i++) {
+//     var deleteBtn = deleteBtn[i];
+//     deleteBtn.onclick = askDeletePost(); 
+
+// }
+
+// document.getElementsByClassName("deleteBtn").onclick = function() {askDeletePost()};
+
+$(document).ready(function () {
+    $('.deleteBtn').click(function() {
+        document.getElementById("overlayDeletePost").style.display = "block";
+    });
+
+    $('.close').click(function() {
+        document.getElementById("overlayDeletePost").style.display = "none";
+    });
+});
+
