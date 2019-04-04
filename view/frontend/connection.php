12112bee1b4a8daa3395c23ac67080d67904945e
Mathias <mathias.lavergne.graph@gmail.com>
2019-04-04 12:23:59 +0200

    Ajout de la page de connexion

---
+++ view/frontend/connection.php
@@ -0,0 +1,23 @@
+<?php 
+
+// namespace Math\projet04;
+
+?>
+
+<?php $title = 'Se connecter'; ?>
+
+
+<h1>Connexion</h1>
+
+<form method="post" action="<?= HOST; ?>login">
+    <div class="form-group">
+        <label for="nom">Nom</label>
+        <input type=text class="form-control" id="nom" name="name">
+    </div>
+    <div class="form-group">
+        <label for="password">Mot de passe</label>
+        <input type=password class="form-control" id="password" name="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Se connecter</button>
+</form>
+
