a332dcb563ae4f1c1e3e0aa9a04bc66c6fea61a2
Mathias <mathias.lavergne.graph@gmail.com>
2019-05-01 19:25:41 +0200

    Correction de l'ancre pour le message de modification d'article

--- controller/PostController.php
+++ controller/PostController.php
@@ -93,7 +93,7 @@ class PostController
 
             // redirect on the updated post
             $view = new View();
-            $view->redirect('post/id/' . $params['id'] . '#actionDone');
+            $view->redirect('post/id/' . $params['id'] . '#anchorPost');
 
         } else {
 
