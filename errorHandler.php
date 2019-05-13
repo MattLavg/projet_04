fbe205ab93f9bcccf7c0047e911ff62ca371a98a
Mathias <mathias.lavergne.graph@gmail.com>
2019-05-13 21:43:04 +0200

    Ajout du fichiet errorHandler.php

---
+++ errorHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+use Blog\Core\MyException;
+
+function errorToException($code, $message, $fichier, $ligne)
+{
+    throw new MyException($message, 0, $code, $fichier, $ligne);
+}
+
+set_error_handler('errorToException');
