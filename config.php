78863ce680eb96a75769c18fe376485c8645fb84
Mathias <mathias.lavergne.graph@gmail.com>
2019-05-13 21:46:23 +0200

    Creation d'une constante de route pour les fichiers contenus dans le dossier core et ajout d'un require dans l'index pour le errorHandler

--- config.php
+++ config.php
@@ -10,11 +10,7 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
 
 define('ROOT', __DIR__ . DIRECTORY_SEPARATOR); // DIR correspond au dossier contenant le fichier qui appelle la constante
 
-define('AUTOLOAD', ROOT . 'model' . DIRECTORY_SEPARATOR);
-
-// define('CONTROLLER', ROOT . 'controller' . DIRECTORY_SEPARATOR);
-// define('MODEL', ROOT . 'model' . DIRECTORY_SEPARATOR);
-// define('APPLICATION', ROOT . 'application' . DIRECTORY_SEPARATOR);
+define('CORE', ROOT . 'core' . DIRECTORY_SEPARATOR);
 
 define('VIEWFRONT', ROOT . 'view' . DIRECTORY_SEPARATOR . 'frontend' . DIRECTORY_SEPARATOR);
 define('VIEWBACK', ROOT . 'view' . DIRECTORY_SEPARATOR . 'backend' . DIRECTORY_SEPARATOR);
--- index.php
+++ index.php
@@ -1,60 +1,27 @@
 <?php
 
-use Blog\Model\Autoloader;
+use Blog\Core\Autoloader;
+use Blog\Core\MyException;
 use Blog\Core\Routeur;
 use Blog\Core\Registry;
 
-class MonException extends ErrorException
-{
-  public function __toString()
-  {
-    switch ($this->severity)
-    {
-      case E_USER_ERROR : // Si l'utilisateur émet une erreur fatale;
-        $type = 'Erreur fatale';
-        break;
-      
-      case E_WARNING : // Si PHP émet une alerte.
-      case E_USER_WARNING : // Si l'utilisateur émet une alerte.
-        $type = 'Attention';
-        break;
-      
-      case E_NOTICE : // Si PHP émet une notice.
-      case E_USER_NOTICE : // Si l'utilisateur émet une notice.
-        $type = 'Note';
-        break;
-      
-      default : // Erreur inconnue.
-        $type = 'Erreur inconnue';
-        break;
-    }
-    
-    return '<strong>' . $type . '</strong> : [' . $this->code . '] ' . $this->message . '<br /><strong>' . $this->file . '</strong> à la ligne <strong>' . $this->line . '</strong>';
-  }
-}
-
-function error2exception($code, $message, $fichier, $ligne)
-{
-    throw new MonException($message, 0, $code, $fichier, $ligne);
-}
-
-set_error_handler('error2exception');
-
-
-
 require_once('config.php');
-require_once(AUTOLOAD . 'Autoloader.php');
+require_once('errorHandler.php');
+require_once(CORE . 'Autoloader.php');
+require_once(CORE . 'MyException.php');
 Autoloader::start();
 
 
 try {
 
-    try {
-        $db = new \PDO('mysql:host=localhost;dbname=projet04;charset=utf8', 'root', 'root');
-        // $db = new \PDO('mysql:host=boudin.o2switch.net;dbname=mathiasla_projet04;charset=utf8', 'mathiasla_p4', 'A_e409-lrd91_hcol');
-    } catch (\Exception $e) {
-        throw new \Exception('Impossible de se connecter à la base de donnée.');
-    }
+    // try {
+    //     $db = new \PDO('mysql:host=localhost;dbname=projet04;charset=utf8', 'root', 'root');
+    //     // $db = new \PDO('mysql:host=boudin.o2switch.net;dbname=mathiasla_projet04;charset=utf8', 'mathiasla_p4', 'A_e409-lrd91_hcol');
+    // } catch (\Exception $e) {
+    //     throw new \Exception('Impossible de se connecter à la base de donnée.');
+    // }
+
+    $db = new \PDO('mysql:host=localhost;dbname=projet04;charset=utf8', 'root', 'root');
     
 
     session_start();
